5ba78a816aff7dde5aa8ad58df63fe37324d319a
MangoShip <mingun0108@gmail.com>
2021-11-28 20:36:25 -0800

    Initial commit

---
+++ example/compile.sh
@@ -0,0 +1,1 @@
+/usr/bin/g++ -DNDEBUG -g -I/home/mingun0108/Desktop/vulkan/x86_64/include -I/home/mingun0108/Desktop/vulkanAndroid/easyvkAndroid/include -std=gnu++14 -o exec vect-add.cpp -leasyvkAndroid -lvulkan
